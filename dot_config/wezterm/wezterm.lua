5526e143801c066071b1a2ed037ebb75c97786ca
Oscar Torresbaca <oscar.torresbaca@nordicsemi.no>
2024-11-11 09:29:14 +0100

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -10,18 +10,20 @@ local config = wezterm.config_builder()
 -- config.color_scheme = 'Batman'
 -- config.color_scheme = 'Later This Evening (Gogh)'
 
+-- DIsable all bindings for debugging
+config.disable_default_key_bindings = false
+
 
 -- config.font = wezterm.font 'Fira Code'
 config.font = wezterm.font 'Hack Nerd Font'
 
-config.hide_tab_bar_if_only_one_tab = true
-
+config.hide_tab_bar_if_only_one_tab = false
 config.window_background_opacity = 0.5
 
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-
-config.window_background_opacity = 0.8
+  config.window_background_opacity = 0.8
+  config.default_domain = 'WSL:Ubuntu'
 end
 
 
@@ -31,7 +33,7 @@ config.command_palette_font_size = 12.0
 config.command_palette_bg_color = "#1f1f1f"
 
 config.command_palette_rows = 10
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 
 
 config.skip_close_confirmation_for_processes_named = {

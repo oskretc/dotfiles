0ed12a18721bd043b3c5c8d171359fe88e44ad32
Oscar Torresbaca <oscar.torresbaca@nordicsemi.no>
2025-01-24 14:50:46 +0100

    Update .local/bin/sql_terminal.sh

--- dot_local/bin/executable_sql_terminal.sh
+++ dot_local/bin/executable_sql_terminal.sh
@@ -3,11 +3,16 @@
 file="/tmp/hextemp.tmp"
 touch $file
 
-zellij run -fc -- bash sql_editor.sh $3
+zellij run -c -n helix --direction down -- bash sql_editor.sh $3
+zellij action move-pane up
 
-while [ -e "$file" ]; do
-  sleep 1 # Wait for 1 second before checking again
-done
+zellij action resize increase up
+zellij action resize increase up
+zellij action resize increase up
+zellij action resize increase up
+# while [ -e "$file" ]; do
+#   sleep 1 # Wait for 1 second before checking again
+# done
 
 
 

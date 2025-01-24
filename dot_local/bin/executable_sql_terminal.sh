416589164343d0e59137a6530ff552e58aad0df2
Oscar Torresbaca <oscar.torresbaca@nordicsemi.no>
2025-01-24 15:33:29 +0100

    Update .local/bin/sql_terminal.sh

--- dot_local/bin/executable_sql_terminal.sh
+++ dot_local/bin/executable_sql_terminal.sh
@@ -4,12 +4,15 @@ file="/tmp/hextemp.tmp"
 touch $file
 
 zellij run -c -n helix --direction down -- bash sql_editor.sh $3
+zellij action write-chars "i"
 zellij action move-pane up
 
 zellij action resize increase up
 zellij action resize increase up
 zellij action resize increase up
 zellij action resize increase up
+
+# when using floating window, the script needs to wait until hx exists
 # while [ -e "$file" ]; do
 #   sleep 1 # Wait for 1 second before checking again
 # done

e8d487db10212dcaffcdf0deaa43273910e41b5c
Oscar Torresbaca <oscar.torresbaca@nordicsemi.no>
2025-02-11 08:43:02 +0100

    Add .local/bin/keepass2fzf.py

---
+++ dot_local/bin/keepass2fzf.py
@@ -0,0 +1,12 @@
+
+from passhole.passhole import open_database
+# from operator import attrgetter as ag
+kp = open_database()
+
+for e in kp.entries:
+    
+    print(f"{e.group}, {e.title}, {e.username}, {e.password}")
+
+
+ 
+

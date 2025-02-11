02053d550d73d6ec1540e3a29f3a949af2639f4d
Oscar Torresbaca <oscar.torresbaca@nordicsemi.no>
2025-02-11 09:59:04 +0100

    Update .local/bin/keepass2fzf.py

--- dot_local/bin/keepass2fzf.py
+++ dot_local/bin/keepass2fzf.py
@@ -3,9 +3,20 @@ from passhole.passhole import open_database
 # from operator import attrgetter as ag
 kp = open_database()
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 for e in kp.entries:
     
-    print(f"{e.group}::{e.title}::{e.username}::{e.password}::")
+    print(f"{e.group}::{bcolors.OKCYAN+e.title+ bcolors.ENDC}::{e.username}::{e.password}::")
 
 
  

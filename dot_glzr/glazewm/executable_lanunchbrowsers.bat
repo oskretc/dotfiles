b83f3f39d45d8534f4215ed5b88b51391ef8df4a
Oscar Torresbaca <oscar.torresbaca@nordicsemi.no>
2025-03-17 09:19:30 +0100

    Add .glzr/glazewm/lanunchbrowsers.bat

---
+++ dot_glzr/glazewm/executable_lanunchbrowsers.bat
@@ -0,0 +1,6 @@
+
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --args --window-name="BROWSER"
+
+start "" "C:\Users\osto\qutebrowser-3.4.0-windows-standalone\qutebrowser-3.4.0\qutebrowser.exe"
+
+# "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --app=https://google.com --args --window-name="MINIBROWSER"

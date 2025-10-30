fe01b025229d0e30944e10635e3f6d832be1fb3c
Oscar Torresbaca <oscar.torresbaca@nordicsemi.no>
2025-10-30 22:17:31 +0100

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -59,5 +59,5 @@ alias cch="cp ~/.config/helix/config.toml ~/.local/share/chezmoi/.chezmoitemplat
 alias lj="lumo"
 alias iss="intelli-shell"
 alias e='hx "$(fd | fzf --preview "bat {}")"'
-alias nl='f() { zellij action new-tab --layout tall2.kdl --name $1 };f'
+alias nl='f() { zellij action new-tab --layout tall.kdl --name $1 };f'
 
